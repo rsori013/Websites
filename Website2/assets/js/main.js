572166f48e21d702e9ea210b35a7576475a2ffd7
rsori013 <rsori013@ucr.edu>
2023-11-12 10:20:32 -0800

    updated README

--- Website2/assets/js/main.js
+++ Website2/assets/js/main.js
@@ -8,15 +8,15 @@ email.addEventListener('input', () => {
    // Evaluates if it matches the pattern values
    if (email.value.match(pattern)) {
       form.classList.add('valid')
-      form.classList.remove('invalid')
+      form.classList.remove('invalids')
    } else {
-      form.classList.add('invalid')
+      form.classList.add('invalids')
       form.classList.remove('valid')
    }
  
    // If the input field is empty, delete classes
    if (email.value == '') {
-      form.classList.remove('invalid')
+      form.classList.remove('invalids')
       form.classList.remove('valid')
    }
 })
